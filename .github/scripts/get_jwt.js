ab6043e41b73e8862666128b2d33c61b3f11bbab
mrmxyzptlk1 <146275395+mrmxyzptlk1@users.noreply.github.com>
2023-10-27 14:30:10 +0200

    Update get_jwt.js

--- .github/scripts/get_jwt.js
+++ .github/scripts/get_jwt.js
@@ -1,4 +1,4 @@
-module.exports = ({github, context, core}) => {
+rnmodule.exports = ({github, context, core}) => {
   const appId = process.env.APPLICATION_ID;
   const privateKey = process.env.APPLICATION_PRIVATE_KEY;
   const installationId = process.env.APPLICATION_INSTALLATION_ID;
@@ -13,6 +13,7 @@ module.exports = ({github, context, core}) => {
   const bearer = jsonwebtoken.sign(payload, privateKey, {algorithm: 'RS256'})
 
   console.log(bearer);
+  console.log("test");
   
-  core.setOutput('result', bearer);
+  return bearer;
 }
