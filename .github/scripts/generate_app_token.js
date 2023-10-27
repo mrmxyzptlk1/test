0d73ee9dbb585375771c14b6d8be50fe0a346971
mrmxyzptlk1 <146275395+mrmxyzptlk1@users.noreply.github.com>
2023-10-27 18:44:50 +0200

    Update generate_app_token.js

--- .github/scripts/generate_app_token.js
+++ .github/scripts/generate_app_token.js
@@ -1,20 +1,13 @@
 module.exports = async ({core, fetch}) => {
+  const { Octokit } = require("@octokit/rest");
+  const { createAppAuth } = require("@octokit/auth-app");
+  
   const installationId = process.env.APPLICATION_INSTALLATION_ID;
   const permissions = process.env.PERMISSIONS;
   const privateKey = process.env.APPLICATION_PRIVATE_KEY;
   const appId = process.env.APPLICATION_ID;
   const repository_id = process.env.REPOSITORY_ID;
 
-  permissions.trim();
-  const perm_array = permissions.split(",");
-  var perm_obj = {};
-  for (var i = 0; i < perm_array.length; i++) {
-    var split = perm_array[i].split(':');
-    perm_obj[split[0].trim()] = split[1].trim();
-  }
-
-  const { Octokit } = require("@octokit/rest");
-  const { createAppAuth } = require("@octokit/auth-app");
   const appOctokit = new Octokit({
     authStrategy: createAppAuth,
     auth: {
@@ -24,14 +17,23 @@ module.exports = async ({core, fetch}) => {
     },
     request: { fetch }
   });
+  
+  permissions.trim();
+  const perm_array = permissions.split(",");
+  var perm_obj = {};
+  for (var i = 0; i < perm_array.length; i++) {
+    var split = perm_array[i].split(':');
+    perm_obj[split[0].trim()] = split[1].trim();
+  }
+
   const data = await appOctokit.auth({
     type: "installation",
     repositoryIds: [ repository_id ],
     permissions: perm_obj
   });
-  console.log(data);
 
-  console.log("Selected permissions: " + JSON.stringify(data.permissions));
+  console.log("Requested token:");
+  console.log(data);
 
   core.setOutput('token', data.token);
 }
