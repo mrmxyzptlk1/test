5c4eca7e59c0890066a5eb7eec860c904c521cf0
mrmxyzptlk1 <146275395+mrmxyzptlk1@users.noreply.github.com>
2023-11-13 03:59:07 +0100

    Update utils.js

--- .github/scripts/utils.js
+++ .github/scripts/utils.js
@@ -3,17 +3,15 @@ module.exports = async ({github, context, utils1, utils2}) => {
   console.log(utils1, utils2);
 
   const action = "create";
-  if (context.actor === 'mrmxyzptlk1') {
+  if (context.actor !== 'mrmxyzptlk1') {
       if (utils1 === '[]' || (utils1 !== '[]' && utils2 === '0')) {
         console.log("Utils: Skip");
         var status = "skip";
       } else if (utils1 !== '[]' && utils2 !== '0') {
         console.log("Utils: Fail");
         var status = "fail";
-      } else {
-        console.log("else");
       }
-  } else if (context.actor !== 'mrmxyzptlk1') {
+  } else if (context.actor === 'mrmxyzptlk1') {
     console.log("Utils: Success");
     var status = "success";
   }
