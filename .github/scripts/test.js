69a95d468b71df037d7b238ab1dec51fd638d964
mrmxyzptlk1 <146275395+mrmxyzptlk1@users.noreply.github.com>
2023-11-13 02:10:13 +0100

    Update test.js

--- .github/scripts/test.js
+++ .github/scripts/test.js
@@ -1,4 +1,4 @@
-module.exports = async ({github, context, value1, value2}) => {
+module.exports = async ({github, context, param3, param4}) => {
   console.log("inisde test.js script:");
-  console.log(value1, value2);
+  console.log(param3, param4);
 }
