6b8584a1535d6912059c5243cba69a35d40789fb
mrmxyzptlk1 <146275395+mrmxyzptlk1@users.noreply.github.com>
2023-11-13 21:25:21 +0100

    Create test_two.js

---
+++ .github/scripts/test_two.js
@@ -0,0 +1,23 @@
+module.exports = async ({github, context, check_value1, check_value2}) => {
+  function sleep(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+  }
+  
+  var counter = 1;
+  async function checkIfMerged() {
+      while (counter < 9) {
+          try {
+              console.log(counter)
+              console.log(test)
+          } catch (error) {
+              await sleep(1000);
+              counter++;
+              if (counter == 9) {
+                console.log("done");
+              }
+          }
+      }
+  }
+  
+  setTimeout(checkIfMerged, 5000);
+}
