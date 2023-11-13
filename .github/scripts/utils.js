abbd768d243efbd385a7d13e2abee214df885b95
mrmxyzptlk1 <146275395+mrmxyzptlk1@users.noreply.github.com>
2023-11-13 02:10:09 +0100

    Update utils.js

--- .github/scripts/utils.js
+++ .github/scripts/utils.js
@@ -1,5 +1,8 @@
-module.exports = async ({github, context}) => {
-  const value1 = "false"
-  const value2 = "true"
+module.exports = async ({github, context, param1, param2}) => {
+  console.log("inisde utils.js script:");
+  console.log(param3, param4);
+  
+  const value1 = "test_value1"
+  const value2 = "test_value2"
   return { value1, value2 }
 }
