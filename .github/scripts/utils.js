f3769e5bbefbc172187cbe1537ded900cdb8b0e9
mrmxyzptlk1 <146275395+mrmxyzptlk1@users.noreply.github.com>
2023-11-13 04:12:55 +0100

    Update utils.js

--- .github/scripts/utils.js
+++ .github/scripts/utils.js
@@ -2,6 +2,7 @@ module.exports = async ({github, context, utils1, utils2}) => {
   console.log("inisde utils.js script:");
   console.log(utils1, utils2);
 
+  console.log(context.eventName);
   if (context.eventName === 'pull_request') {
     var action = "create";
     if (context.actor !== 'mrmxyzptlk1') {
