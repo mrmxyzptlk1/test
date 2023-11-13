5f3044e05983b02ad8acb914ff95333ea122212a
mrmxyzptlk1 <146275395+mrmxyzptlk1@users.noreply.github.com>
2023-11-13 02:23:27 +0100

    Update utils.js

--- .github/scripts/utils.js
+++ .github/scripts/utils.js
@@ -4,5 +4,5 @@ module.exports = async ({github, context, utils1, utils2}) => {
   
   const value1 = "test_value1"
   const value2 = "test_value2"
-  return { value1, value2 }
+  return [value1, value2]
 }
