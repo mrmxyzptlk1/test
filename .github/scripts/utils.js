6014c66167f1f932016244be86751b68cbecd8cc
mrmxyzptlk1 <146275395+mrmxyzptlk1@users.noreply.github.com>
2023-11-13 02:49:19 +0100

    Update utils.js

--- .github/scripts/utils.js
+++ .github/scripts/utils.js
@@ -1,6 +1,18 @@
 module.exports = async ({github, context, utils1, utils2}) => {
   console.log("inisde utils.js script:");
   console.log(utils1, utils2);
+
+  if (context.actor !== 'mrmxyzptlk1') {
+      if (1 > 0 && 2 < 1) {
+        // block of code to be executed if condition1 is true
+      } else if (condition2) {
+        // block of code to be executed if the condition1 is false and condition2 is true
+      } else {
+        // block of code to be executed if the condition1 is false and condition2 is false
+      }
+  } else {
+    console.log("wrong actor")
+  }
   
   const value1 = "test_value1"
   const value2 = "test_value2"
