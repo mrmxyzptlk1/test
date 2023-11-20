8f699b9ad8649706acf6dd0ff4b39278453666a8
mrmxyzptlk1 <146275395+mrmxyzptlk1@users.noreply.github.com>
2023-11-20 19:02:20 +0100

    Update index.js

--- .github/scripts/index.js
+++ .github/scripts/index.js
@@ -2,6 +2,6 @@
 //const test2 = require('./.github/scripts/test2.js');
 
 module.exports = {
-  test,
-  test2
+  test: require('./.github/scripts/test.js'),
+  test2: require('./.github/scripts/test2.js')
 }
